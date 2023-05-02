e77b9574cbfeaab86ddbd576dd077c486b50795f
dominikmartinez <dominik@martinez.cc>
2023-05-02 11:00:11 +0200

    push experiment state (add bpe/detokenizer, restrict wandb logging to training phase)

--- fairseq_easy_extend/criterions/rl_criterion.py
+++ fairseq_easy_extend/criterions/rl_criterion.py
@@ -8,14 +8,14 @@ from fairseq.criterions import FairseqCriterion, register_criterion
 from fairseq.dataclass import FairseqDataclass
 from fairseq import utils
 from fairseq.logging import metrics
-#from fairseq.criterions.nat_loss import LabelSmoothedDualImitationCriterion
+from fairseq.data import encoders
+
+from argparse import Namespace
 
 from dataclasses import dataclass, field
 
 import wandb
 
-#from torch.utils.tensorboard import SummaryWriter
-#writer = SummaryWriter()
 
 @dataclass
 class RLCriterionConfig(FairseqDataclass):
@@ -29,6 +29,7 @@ class RLCriterion(FairseqCriterion):
         super().__init__(task)
         self.metric = sentence_level_metric
         self.initialize_sacrebleu()
+        self.tokenizer = encoders.build_tokenizer(Namespace(tokenizer="moses"))
 
     def initialize_sacrebleu(self):
         if self.metric == "BLEU":
@@ -60,8 +61,9 @@ class RLCriterion(FairseqCriterion):
             sampled_indices = torch.multinomial(output_probs, num_samples=1)
             
             # get strings
-            sampled_string = self.task.target_dictionary.string(sampled_indices)
-            target_string = self.task.target_dictionary.string(targets)
+            sampled_string = self.task.target_dictionary.string(sampled_indices, bpe_symbol="subword_nmt")
+            sampled_string = self.tokenizer.decode(sampled_string)
+            target_string = self.task.target_dictionary.string(targets, bpe_symbol="subword_nmt")
             
             # scoring
             with torch.no_grad():
@@ -86,8 +88,9 @@ class RLCriterion(FairseqCriterion):
             sampled_indices = sampled_indices_flat.view(B, T)
 
             # get strings
-            sampled_strings = [self.task.target_dictionary.string(sentence) for sentence in sampled_indices.tolist()]
-            target_strings = [self.task.target_dictionary.string(sentence) for sentence in targets.tolist()]
+            sampled_strings = [self.task.target_dictionary.string(sentence, bpe_symbol="subword_nmt") for sentence in sampled_indices.tolist()]
+            sampled_strings = [self.tokenizer.decode(string) for string in sampled_strings]
+            target_strings = [self.task.target_dictionary.string(sentence, bpe_symbol="subword_nmt") for sentence in targets.tolist()]
 
             # reward calculation
             with torch.no_grad():
@@ -108,8 +111,8 @@ class RLCriterion(FairseqCriterion):
             return loss, reward
             
         
-        return mask_then_sample(outputs, targets, masks)
-        #return sample_then_mask(outputs, targets, masks)
+        #return mask_then_sample(outputs, targets, masks)
+        return sample_then_mask(outputs, targets, masks)
 
     def forward(self, model, sample, reduce=True):
         """Compute the loss for the given sample.
@@ -134,17 +137,29 @@ class RLCriterion(FairseqCriterion):
 
         loss, reward = self._compute_loss(outs, tgt_tokens, masks)
 
+        # we only want to log loss and reward to wandb during training, not during eval
+        # more handling for that in reduce_metrics()
+        if model.training == True:
+            detached_loss = loss.detach()
+            loss_to_be_logged = loss.detach()
+            reward_to_be_logged = reward
+        else:
+            detached_loss = loss.detach()
+            loss_to_be_logged = None
+            reward_to_be_logged = None
+
         # NOTE:
         # we don't need to use sample_size as denominator for the gradient
         # here sample_size is just used for logging
         sample_size = 1
         logging_output = {
-            "loss": loss.detach(),
-            "nll_loss": loss.detach(),
+            "loss": detached_loss,
+            "loss_to_be_logged": loss_to_be_logged,
+            "nll_loss": detached_loss,
             "ntokens": ntokens,
             "nsentences": nsentences,
             "sample_size": sample_size,
-            "reward": reward
+            "reward": reward_to_be_logged
         }
 
         return loss, sample_size, logging_output
@@ -153,18 +168,24 @@ class RLCriterion(FairseqCriterion):
     def reduce_metrics(logging_outputs) -> None:
         """Aggregate logging outputs from data parallel training."""
         loss_sum = sum(log.get("loss", 0) for log in logging_outputs)
+        loss_items = [log.get("loss_to_be_logged", 0) for log in logging_outputs]
+        loss_items_excl_None = [item for item in loss_items if item is not None]
         ntokens = sum(log.get("ntokens", 0) for log in logging_outputs)
         sample_size = sum(log.get("sample_size", 0) for log in logging_outputs)
-        reward_sum = sum(log.get("reward", 0) for log in logging_outputs)
+        reward_items = [log.get("reward", 0) for log in logging_outputs]
+        reward_items_excl_None = [item for item in reward_items if item is not None]
 
         # we divide by log(2) to convert the loss from base e to base 2
         metrics.log_scalar(
             "loss", loss_sum / sample_size / math.log(2), sample_size, round=3
         )
 
-        loss = loss_sum / sample_size / math.log(2)
-        reward = reward_sum / sample_size
-        wandb.log({"loss": loss, "reward": reward})
+        # we only want to log loss and reward to wandb during training, not during eval
+        # more handling for that in forward()
+        if len(loss_items_excl_None) > 0:
+            wandb_loss = sum(loss_items_excl_None) / len(loss_items_excl_None) / math.log(2)
+            wandb_reward = sum(reward_items_excl_None) / len(reward_items_excl_None)
+            wandb.log({"loss": wandb_loss, "reward": wandb_reward})
 
         if sample_size != ntokens:
             metrics.log_scalar(
